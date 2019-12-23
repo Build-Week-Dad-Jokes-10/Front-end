b0cc62157295d3a249b73b88819775edf174ca2e
Derek <legato_5@yahoo.com>
2019-12-22 19:57:40 -0800

    added styling to login form.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { axiosWithAuth } from '../utils/AxiosWithAuth';
+import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
 const Login = (props) => {
     const [cred, setCred] = useState({username: '', password: ''});
+    const [err, setErr] = useState('');
 
     const handleSubmit = e => {
         e.preventDefault();
@@ -10,6 +12,10 @@ const Login = (props) => {
             .then(res => {
                 localStorage.setItem('token', res.data.token);
                 props.history.push('/');
+            })
+            .catch(err => {
+                console.log(err.message);
+                setErr('Invalid Username/Password')
             })
     }
     const handleChange = e => {
@@ -20,25 +26,40 @@ const Login = (props) => {
         })
     }
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Log In</label>
-                <input
-                    type='text'
-                    name='username'
-                    value={cred.username}
-                    onChange={handleChange}
-                />
-                <input
-                    type='password'
-                    name='password'
-                    value={cred.password}
-                    onChange={handleChange}
-                />
-                <p>Don't have an account? <a className="signup-prompt" onClick={()=> {props.history.push('/signup')}}>Sign Up</a></p>
-                <button>Submit</button>
-            </form>
-        </div>
+        <>
+            <h1 className="login-header">Dad Jokes</h1>
+            <Form onSubmit={handleSubmit} className="login-form">
+                <label className="login-label">Log In</label>
+            
+                <div>
+                    <FormGroup className="login-cred">
+                        <Input
+                            type='text'
+                            name='username'
+                            placeholder='Username'
+                            value={cred.username}
+                            onChange={handleChange}
+                        />
+                    </FormGroup>
+            
+                    <FormGroup className="login-cred">
+                        <Input
+                            type='password'
+                            name='password'
+                            placeholder='Password'
+                            value={cred.password}
+                            onChange={handleChange}
+                        />
+                    </FormGroup>
+                </div>
+
+                <FormGroup className="submit-container"> 
+                    <p className="signup-prompt-con">Don't have an account? <span className="signup-prompt" onClick={()=> {props.history.push('/signup')}}>Sign Up</span></p>
+                    <Button>Submit</Button>
+                </FormGroup> 
+                {err && <div className="error-message">{err}</div>}
+            </Form>
+        </>
     )
 }
 
