a23d9e31ad4f0cc13d02f9550983938efbc83309
Cody-Hayes97 <hayesmichael2@gmail.com>
2019-12-22 21:29:49 -0800

    functionality on home page and styles

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,13 +2,34 @@ import React, { useState, useContext } from "react";
 import axios from "axios";
 import { JokeContext } from "../contexts/JokeContext";
 import JokeCardList from "./JokeCardList";
+import { Container, Card, Nav, Navbar, NavItem, NavLink } from "reactstrap";
 
 export default function HomePage() {
   const { joke } = useContext(JokeContext);
   console.log(joke);
   return (
-    <div>
+    <Container>
+      <Card style={{ height: "30vh", marginBottom: "10%" }}>
+        <h1 style={{ paddingTop: "8%", fontSize: "50px" }}>Dad Jokes</h1>
+        <Nav className="d-flex justify-content-center">
+          <NavItem className="home-page-nav">
+            <NavLink href="#" className="home-page-nav-link">
+              Log In
+            </NavLink>
+          </NavItem>
+          <NavItem className="home-page-nav">
+            <NavLink href="#" className="home-page-nav-link">
+              Sign Up
+            </NavLink>
+          </NavItem>
+          <NavItem className="home-page-nav">
+            <NavLink href="#" className="home-page-nav-link">
+              Settings
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Card>
       <JokeCardList />
-    </div>
+    </Container>
   );
 }
--- client/src/components/JokeCard.js
+++ client/src/components/JokeCard.js
@@ -1,18 +1,43 @@
 import React, { useState } from "react";
+import { Card, CardText, Col, Button } from "reactstrap";
 
 export default function JokeCard(props) {
   const [isClicked, setIsClicked] = useState(false);
   return (
-    <div>
-      <p>{props.setup}</p>
-      <button
-        onClick={() => {
-          setIsClicked(!isClicked);
-        }}
+    <Col md={{ offset: 2 }}>
+      <Card
+        style={{ width: "80%", margin: "2% 0", backgroundColor: "#2670C5" }}
       >
-        {!isClicked ? "Show punchline" : "Hide Punchline"}
-      </button>
-      {isClicked ? <p>{props.punchline}</p> : null}
-    </div>
+        <CardText
+          style={{
+            fontSize: "20px",
+            textShadow: "1px 1px black",
+            color: "white",
+            padding: "3%"
+          }}
+        >
+          {props.setup}
+        </CardText>
+        {isClicked ? (
+          <CardText
+            style={{
+              fontSize: "20px",
+              textShadow: "1px 1px black",
+              color: "white"
+            }}
+          >
+            {props.punchline}
+          </CardText>
+        ) : null}
+        <button
+          className="punchline-btn"
+          onClick={() => {
+            setIsClicked(!isClicked);
+          }}
+        >
+          {!isClicked ? "Show punchline" : "Hide Punchline"}
+        </button>
+      </Card>
+    </Col>
   );
 }
