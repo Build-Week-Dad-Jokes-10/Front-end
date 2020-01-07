7ff68437999929b7af8090547c7f2c0ce428bfae
Derek <legato_5@yahoo.com>
2020-01-06 19:20:14 -0800

    added modal for username

--- client/src/components/Settings.js
+++ client/src/components/Settings.js
@@ -1,17 +1,21 @@
 import React, { useState, useContext } from 'react';
 import { axiosWithAuth } from '../utils/AxiosWithAuth';
 import { UserContext }  from '../contexts/UserContext';
-import { Container, Card, Button } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Form, FormGroup, Input, Container, Label, Card, Button } from 'reactstrap';
 import MainHeader from './MainHeader';
 
 const Settings = props => {
     const { user, setUser } = useContext(UserContext);
+    const [newData, setNewData] = useState({username: '', password: ''})
     const buttonStyle = {
         marginTop: '3%',
         backgroundColor: "#EBC700",
         color: "#380a15",
         fontWeight: "bold"
     }
+
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal)
     return (
         <>
             <MainHeader />
@@ -30,8 +34,23 @@ const Settings = props => {
                         justifyContent: 'space-evenly'
                     }}>
                         <Button
-                        style={buttonStyle}
+                        style={buttonStyle} onClick={toggle}
                         >Change your Username</Button>
+                        <Modal isOpen={modal} toggle={toggle} className="change-user">
+                            <ModalHeader toggle={toggle}>Change Username</ModalHeader>
+                            <Form onSubmit={'handleSubmit'}>
+                            <FormGroup>
+                                <Label>Change your Username</Label>
+                                <Input
+                                    type='text'
+                                    name="setup"
+                                    placeholder="Username"
+                                    value={newData.username}
+                                />
+                                </FormGroup>
+                                <Button>Submit</Button>
+                            </Form>
+                        </Modal>
                         <Button style={buttonStyle}>Change your Password</Button>
                     </div>
                 </Card>
