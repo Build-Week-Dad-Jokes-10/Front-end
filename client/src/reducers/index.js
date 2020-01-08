eb7da1c6f3321b902da7c69aeab6141f440f1ae8
Derek <legato_5@yahoo.com>
2020-01-07 17:43:48 -0800

    created reducer cases for login start and success

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,12 +1,28 @@
 export const initialState = {
   loggedIn: false,
+  isLoading: false,
   error: '',
-  username: '',
-  password: ''
+  user: {
+    username: '',
+    password: ''
+  }
 }
 
 export const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case "LOGIN_START":
+      return {
+        ...state,
+        error: '',
+        isLoading: true
+      }
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        loggedIn: true,
+        isLoading: false,
+        user: action.payload
+      }
     default:
       return state;
   }
