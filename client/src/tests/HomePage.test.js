9e909a991110c7c871b9177d680788a3727a342d
Cody-Hayes97 <hayesmichael2@gmail.com>
2020-01-12 18:08:33 -0800

    tried to fix tests

--- client/src/tests/HomePage.test.js
+++ client/src/tests/HomePage.test.js
@@ -2,10 +2,17 @@ import React from "react";
 import * as rtl from "@testing-library/react";
 import HomePage from "../components/HomePage";
 import "@testing-library/jest-dom/extend-expect";
+import { act } from "react-dom/test-utils";
+import { shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Enzyme from "enzyme";
 
-test("renders header and nav", () => {
-  const wrapper = rtl.render(<HomePage />);
+Enzyme.configure({ adapter: new Adapter() });
+describe("render pls", () => {
+  it("shallow renders", () => {
+    const wrapper = shallow(<HomePage />);
+    const head = wrapper.getByText(/Dad/gi);
 
-  const header = wrapper.getByText(/Dad/i);
-  expect(header).toBeVisible();
+    expect(head).ToBeVisible();
+  });
 });
--- client/src/tests/Login.test.js
+++ client/src/tests/Login.test.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import * as rtl from '@testing-library/react';
-import Login from '../components/Login';
+import React from "react";
+import * as rtl from "@testing-library/react";
+import { Login } from "../components/Login";
 import "@testing-library/jest-dom/extend-expect";
+import { act } from "react-dom/test-utils";
+import JokeCardList from "../components/JokeCardList";
+import { UserContext } from "../contexts/UserContext";
+import Enzyme, { mount } from "enzyme";
 
+// beforeEach(() => {
+//   jest.resetModules();
+// });
 
-test('render Login form', () => {
-    const wrapper = rtl.render(<Login user={{username: '', password: ''}}/>);
+// const user = (
+//   context = {
+//     state: {
+//       username: "",
+//       password: ""
+//     }
+//   }
+// ) => {
+//   jest.doMock("../contexts/UserContext", () => {
+//     return {
+//       UserContext: {
+//         Consumer: props => props.children(context)
+//       }
+//     };
+//   });
 
-    const form = wrapper.getByTestId(/login-form/i);
-    expect(form).toBeVisible();
-})
+//   return require("../components/Login").Login;
+// };
+
+test("should render login page", () => {
+  const wrapper = rtl.render(<Login />);
+
+  const form = wrapper.getByTestId(/login-form/gi);
+  expect(form).toBeVisible();
+});
